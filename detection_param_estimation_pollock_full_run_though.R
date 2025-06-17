76e9acd5be38a03662bab2526115868eed1b9724
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2025-06-17 08:49:07 -0700

    Remove dependency on tidyverse and cleanup

--- detection_param_estimation_pollock_full_run_though.R
+++ detection_param_estimation_pollock_full_run_though.R
@@ -5,7 +5,6 @@
 
 rm(list = ls())
 library(likelihoodExplore)
-library(tidyverse)
 library(optimx)
 
 
@@ -24,7 +23,7 @@ vol_func <- get_vol_func(matcal)
 
 # read in fish data
 targets<-read.csv("detection_function/targets.csv",header=TRUE)
-targets=targets %>% filter(SPECIES_GROUP=="Adult_pollock")
+targets=targets |> subset(SPECIES_GROUP=="Adult_pollock")
 targets$RANGE=targets$RANGE/100# change units from cm to m
 
 
@@ -42,19 +41,29 @@ params=c(vol_func$p_vol,out$p1,out$p2)
 # typical slope intercept of the logistic regression.  The third parameter in the optimization is the scale paramter,
 # we ignore it so that the function maximizes at 1
 # define the complete function - volume 2nd order polynomial and the logistic detection function
-eff_vol_func<- function(x,params){(params[1]+params[2]*x+params[3]*x^2)*(1./(1+9^((params[4]-x)/params[5])))}
+eff_vol_func<- function(x,params){
+  vol_func$vol_func(x)*(1./(1+9^((params[4]-x)/params[5])))
+}
+
+xseq <- seq(0,15, len=1000)
+plot(xseq, (params[1]+params[2]*xseq+params[3]*xseq^2))
+plot(xseq, (1./(1+9^((params[4]-xseq)/params[5]))))
+plot(xseq, eff_vol_func(xseq, params))
 
 # integrate
 eff_vol=integrate(eff_vol_func,params,lower =0, upper =15)
 
 # now we can turn the counts-by-frame into density-by-frame
-count_targets=targets %>% group_by(FRAME_NUMBER) %>% summarise(COUNT=n()) %>% mutate(DENSITY=COUNT/eff_vol$value)
+count_targets <- aggregate(FRAME_NUMBER ~ FRAME_NUMBER, data = targets, FUN = length)
+count_targets <- as.data.frame(table(targets$FRAME_NUMBER), stringsAsFactors = FALSE)
+names(count_targets) <- c("FRAME_NUMBER", "COUNT")
+count_targets$FRAME_NUMBER <- as.numeric(count_targets$FRAME_NUMBER)
+count_targets$DENSITY <- with(count_targets, COUNT/eff_vol$value)
+
 # need to adjust this for "empty" frames
-all_frames=seq(min(count_targets$FRAME_NUMBER),max(count_targets$FRAME_NUMBER))
-empty_frames=data.frame("FRAME_NUMBER"=setdiff(all_frames,count_targets$FRAME_NUMBER))
-empty_frames$COUNT=0
-empty_frames$DENSITY=0
-count_targets=rbind(count_targets,empty_frames)
+all_frames <- with(count_targets, seq(min(FRAME_NUMBER),max(FRAME_NUMBER)))
+empty_frames <- data.frame("FRAME_NUMBER"=setdiff(all_frames,count_targets$FRAME_NUMBER), COUNT=0, DENSITY=0)
+count_targets <- rbind(count_targets,empty_frames)
 summary(count_targets)
 print(sum(count_targets$DENSITY))
 # 4.462108 should match if nothing changed
